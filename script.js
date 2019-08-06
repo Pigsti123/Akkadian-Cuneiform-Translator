43ec2e723c171e562e9ae1331065a9baaeb07fa1
TexRexTex2 <52430158+TexRexTex2@users.noreply.github.com>
2019-08-07 09:13:48 +1200

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ var node_list = document.getElementsByTagName('input');
 for (var i = 0; i < node_list.length; i++) {
     var node = node_list[i];
         if(node.getAttribute('type') == "translator"){
-          ST();
             node.style.display = "none";
+            ST();
         }
 } 
 }
