26456904c8b5e6da3ebe22f39ba91142c8af5759
TexRexTex <40045439+TexRexTex@users.noreply.github.com>
2019-08-07 09:21:20 +1200

    Update script.js

--- script.js
+++ script.js
@@ -14,10 +14,12 @@ function ST(){
     var output = document.createElement("TEXTAREA");
     input.style.height="100px";
     input.style.width="49%";
-    input.style.resize = "none";
+    input.style.resize="none";
+    input.placeholder="Text";
     output.style.height="100px";
     output.style.width="49%";
-    output.style.resize = "none";
+    output.style.resize="none";
+    output.placeholder="Translation";
     document.body.appendChild(input);
     document.body.appendChild(output);
 }
