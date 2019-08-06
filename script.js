2465a6c2d5306bc6c5389feca6efa7fc6995b671
TexRexTex <40045439+TexRexTex@users.noreply.github.com>
2019-08-06 17:01:00 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-$(":translator").append("<textarea></textarea>");
+var node_list = document.getElementsByTagName('input');
+for (var i = 0; i < node_list.length; i++) {
+    var node = node_list[i];
+        if(node.getAttribute('type') == "translator"){
+          ST();
+        }
+} 
+
+function st(){
+  var input = document.createElement("TEXTAREA");
+  var output = document.createElement("TEXTAREA");
+  document.body.appendChild(input);
+  document.body.appendChild(output);
+}
