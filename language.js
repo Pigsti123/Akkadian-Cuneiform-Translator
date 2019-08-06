f4566f334dc2d47471e4fca8f0026c105bc466b2
Pigsti123 <40046128+Pigsti123@users.noreply.github.com>
2019-08-07 09:57:01 +1200

    Update language.js

--- language.js
+++ language.js
@@ -1,3 +1,15 @@
 var language = {
-//word: "translation"
+//word: "translation",
+  alas: "â",
+  bind: "aʾālu",
+  grandfather: "ab abi",
+  lead: "abāku",
+  dry: "ablu",
+  dried: "abalu",
+  arid: "ablu",
+  hay: "ablūtu",
+  sea: "abamātu",
+  alum: "aban gabe",
+  love-charm: "aban rāmi",
+  
 };
